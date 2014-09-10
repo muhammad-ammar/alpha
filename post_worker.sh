da1c0d81bb11efb2686c2f0d6ff8a8f4fffe810e
MuhammadAmmar <mammar@gmail.com>
2014-09-11 02:57:57 +0500

    fix

---
+++ post_worker.sh
@@ -0,0 +1,14 @@
+current_path=`pwd`
+
+echo $current_path
+
+reports_path=$current_path/reports
+
+echo $reports_path
+
+dest_path=$HOME/results/$TDDIUM_SESSION_ID/session/
+
+echo $dest_path
+
+#cp -rf $reports_path $dest_path 
+
