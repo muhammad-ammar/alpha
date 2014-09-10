62b460ce42a35b6853b9526ec180da07d5a15809
MuhammadAmmar <mammar@gmail.com>
2014-09-11 03:06:23 +0500

    fix

--- post_worker.sh
+++ post_worker.sh
@@ -10,5 +10,12 @@ dest_path=$HOME/results/$TDDIUM_SESSION_ID/session/
 
 echo $dest_path
 
+ls $dest_path
+
 cp -rf $reports_path $dest_path 
 
+echo '*************************************'
+
+ls $dest_path
+
+echo 'DONE'
