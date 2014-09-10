fe68cca0d7a6c40234a49a4f4a4b6fdbafad7411
MuhammadAmmar <mammar@gmail.com>
2014-09-11 01:56:46 +0500

    fix

--- post_worker.py
+++ post_worker.py
@@ -1,24 +1,12 @@
-import tarfile
 import os
 import shutil
 
 full_path = os.path.realpath(__file__)
-source_dir = full_path.replace("post_worker.py", "reports/")
-output_filename = full_path.replace("post_worker.py", "reports.tar.gz")
+source_path = full_path.replace("post_worker.py", "reports/diff_quality_pep8.html")
 
-print "source dir:", source_dir
+print "full_path:", full_path
+print "source dir:", source_path
 
-count = 0
-
-# walk through every subdirectory & add the folder if it is not empty
-with tarfile.open(output_filename, "w:gz") as tar:
-    for (path, dirs, files) in os.walk(source_dir):
-        if len(files) > 0:
-            print "tarring:", path
-            tar.add(path, arcname=os.path.basename(path))
-            count += 1
-
-tar.close()
 
 session_path = os.path.join(
     os.environ['HOME'],
@@ -26,12 +14,10 @@ session_path = os.path.join(
     os.environ['TDDIUM_SESSION_ID'],
     'session')
 
-file_dest = os.path.join(session_path, 'reports.tar.gz')
+file_dest = os.path.join(session_path, 'diff_quality_pep8.html')
 
-# if the tar file is not empty, copy it to the proper place
-if count > 0:
-    print 'copying tar file to:', file_dest
-    shutil.copyfile(output_filename, file_dest)
+print 'copying diff_quality_pep8.html to:', file_dest
+shutil.copyfile(source_path, file_dest)
 
 # finding if there is any screenshot or log file
 #print 'attaching failed screenshots and logs (if any)'
