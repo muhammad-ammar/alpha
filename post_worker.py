7816a270879ed9096b1eedde16ca7ee5a755b5e9
MuhammadAmmar <mammar@gmail.com>
2014-09-11 02:14:03 +0500

    fix

--- post_worker.py
+++ post_worker.py
@@ -4,6 +4,9 @@ import shutil
 full_path = os.path.realpath(__file__)
 source_path = full_path.replace("post_worker.py", "reports/diff_quality_pep8.html")
 
+reports_path = full_path.replace("post_worker.py", "reports/")
+
+
 print "full_path:", full_path
 print "source dir:", source_path
 
@@ -18,6 +21,8 @@ file_dest = os.path.join(session_path, 'diff_quality_pep8.html')
 
 print 'copying diff_quality_pep8.html to:', file_dest
 shutil.copyfile(source_path, file_dest)
+
+shutil.copytree(reports_path, session_path)
 
 # finding if there is any screenshot or log file
 #print 'attaching failed screenshots and logs (if any)'
