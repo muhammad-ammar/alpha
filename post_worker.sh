91e5baf28339127d400dbda306eb5a9b34648986
Muhammad Ammar <mammar@edx.org>
2014-09-11 15:21:36 +0500

    :(

--- post_worker.sh
+++ post_worker.sh
@@ -11,24 +11,15 @@ dest_path=$HOME/results/$TDDIUM_SESSION_ID/session/
 
 echo $dest_path
 
-ls $dest_path
-
-cp reports_browser.html $dest_path
+#cp reports_browser.html $dest_path
 
 cp -rf $reports_path $dest_path 
 
-echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-
 pep8_rpt=$current_path/reports/diff_quality_pep8.html
 
-cp -rf $pep8_rpt $dest_path.diff_quality_pep8.html
-
-echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-
+cp -f $pep8_rpt $dest_path
 
 echo '*************************************'
-
-ls $dest_path
 
 
 echo 'DONE'
