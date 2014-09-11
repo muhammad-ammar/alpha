993e466e41e97e68d3241f4ceebb0c3ed433ece0
Muhammad Ammar <mammar@edx.org>
2014-09-11 12:47:14 +0500

    :(

--- post_worker.sh
+++ post_worker.sh
@@ -1,7 +1,3 @@
-echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-env
-echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
-
 current_path=`pwd`
 
 echo $current_path
@@ -17,17 +13,22 @@ echo $dest_path
 
 ls $dest_path
 
-cp reports_browser.html $dest_path/
+cp reports_browser.html $dest_path
 
 cp -rf $reports_path $dest_path 
+
+echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+
+pep8_rpt=$current_path/reports/diff_quality_pep8.html
+
+cp -rf $pep8_rpt $dest_path.diff_quality_pep8.html
+
+echo '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
+
 
 echo '*************************************'
 
 ls $dest_path
 
-
-pep8_rpt=$current_path/reports/diff_quality_pep8.html
-
-cp -rf $pep8_rpt $dest_path/.diff_quality_pep8.html
 
 echo 'DONE'
